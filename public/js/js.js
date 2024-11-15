d1b1dd2b839df8b2c6f8cbde5dc43f1279b91f12
amandafralve <amanda.fralve@gmail.com>
2024-11-14 21:58:41 -0300

    style: restruturando formulários

--- public/js/js.js
+++ public/js/js.js
@@ -4,3 +4,19 @@ const month = date.getMonth() + 1
 const year = date.getFullYear()
 const currentDate = document.querySelector('.currentDate')
 currentDate.innerHTML = `Novos pedidos foram realizados desde o último acesso ao sistema. Hoje é <strong>${day}/${month}/${year}</strong>.`
+
+function tipoPessoa(){
+    let fisica = document.getElementById("fisica");
+    let juridica = document.getElementById("juridica");
+    let formFisica = document.getElementById("formFisica");
+    let formJuridica = document.getElementById("formJuridica");
+
+    if(fisica.checked){
+        formFisica.style.display = "block";
+        formJuridica.style.display = "none";
+    }else if(juridica.checked){
+        formFisica.style.display = "none";
+        formJuridica.style.display = "block";
+
+    }
+}
